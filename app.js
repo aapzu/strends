e2c07e693ec300f9cb4ae83288f9c67197eca774
Aapzu <aapeli.haanpuu@unifina.com>
2015-11-21 23:09:23 +0200

    stylesheet changed. basic template created

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var cons = require('consolidate');
-var routes = require('./routes/index');
+
+// The route files must me required here
+var index = require('./routes/index');
+
 var app = express();
+var base = express.Router();
 
 require('underscore-express')(app);
 
@@ -19,9 +23,11 @@ app.set('view engine', 'html');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/strends', base);
+app.use('/strends', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
